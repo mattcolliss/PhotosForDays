4404c624b84b59709b76338140faa1a0794f4209
Matth Colliss <mattccolliss@gmail.com>
2020-06-15 12:46:08 +0100

    Adds stat restoration constants

--- PhotosForDays/Utils/Constants.swift
+++ PhotosForDays/Utils/Constants.swift
@@ -8,6 +8,11 @@
 
 import Foundation
 
+// Flickr API
 let flickrApiBaseUrl = URL(string: "https://www.flickr.com")!
 let flickrApiKey = "e45c170dab0e8894f1fd0624af1de35c"
 let flickrPhotoUrlFormat = "https://farm%1$@.staticflickr.com/%2$@/%3$@_%4$@.jpg"
+
+// State Restoration
+let selectDateActivityType = "com.collissions.PhotosForDays.selectDate"
+let selectPhotoActivityType = "com.collissions.PhotosForDays.selectPhoto"
